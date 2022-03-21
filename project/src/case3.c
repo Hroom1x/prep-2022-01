54245cabfc2631bb52ecac3f5c7c4899f7ccca70
Mikhail T <mihatjgl@mail.ru>
2022-03-21 13:45:41 +0300

    Сделал проще

--- project/src/case3.c
+++ project/src/case3.c
@@ -3,9 +3,7 @@
 #include "case3.h"
 
 int is_prime(int num) {
-    if (num == 1 || num == 0) {
-        return 0;
-    } else if (num < 0) {
+    if (num < 2) {
         return 0;
     } else {
         for (int i = num - 1; i > 1; --i) {
