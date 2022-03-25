2e301c42e0903fc5b8306f1069f7a721c254e8bd
Mikhail T <mihatjgl@mail.ru>
2022-03-26 02:19:39 +0300

    addres ---> address

--- project/src/main.c
+++ project/src/main.c
@@ -5,7 +5,7 @@ struct masterRecord {
     int Number;
     char Name[20];
     char Surname[20];
-    char addres[30];
+    char address[30];
     char TelNumber[15];
     double indebtedness;
     double credit_limit;
@@ -13,13 +13,11 @@ struct masterRecord {
 };
 typedef struct masterRecord Data;
 
-int main(void) {
+int main() {
     int choice = 0;
-    void masterWrite(FILE *ofPTR, Data Client), transactionWrite(FILE *ofPTR, Data transfer), blackRecord(FILE *ofPTR,
-                                                                                                          FILE *ofPTR_2,
-                                                                                                          FILE *blackrecord,
-                                                                                                          Data client_data,
-                                                                                                          Data transfer);
+    void masterWrite(FILE *ofPTR, Data Client);
+    void transactionWrite(FILE *ofPTR, Data transfer);
+    void blackRecord(FILE *ofPTR, FILE *ofPTR_2, FILE *blackrecord, Data client_data, Data transfer);
     FILE *Ptr, *Ptr_2, *blackrecord;
     Data client_data, transfer;
     printf("%s", "please enter action\n1 enter data client:\n2 enter data transaction:\n3 update base\n");
@@ -73,20 +71,20 @@ void masterWrite(FILE *ofPTR, Data Client) {
            "1 Number account: ",
            "2 Client name: ",
            "3 Surname: ",
-           "4 Addres client: ",
+           "4 address client: ",
            "5 Client Telnum: ",
            "6 Client indebtedness: ",
            "7 Client credit limit: ",
            "8 Client cash payments: ");
-    while (scanf("%d%s%s%s%s%lf%lf%lf", &Client.Number, Client.Name, Client.Surname, Client.addres, Client.TelNumber,
+    while (scanf("%d%s%s%s%s%lf%lf%lf", &Client.Number, Client.Name, Client.Surname, Client.address, Client.TelNumber,
                  &Client.indebtedness, &Client.credit_limit, &Client.cash_payments) != -1) {
         fprintf(ofPTR, "%-12d%-11s%-11s%-16s%20s%12.2f%12.2f%12.2f\n", Client.Number, Client.Name, Client.Surname,
-                Client.addres, Client.TelNumber, Client.indebtedness, Client.credit_limit, Client.cash_payments);
+                Client.address, Client.TelNumber, Client.indebtedness, Client.credit_limit, Client.cash_payments);
         printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n",
                "1 Number account: ",
                "2 Client name: ",
                "3 Surname: ",
-               "4 Addres client: ",
+               "4 address client: ",
                "5 Client Telnum: ",
                "6 Client indebtedness: ",
                "7 Client credit limit: ",
@@ -112,7 +110,7 @@ void transactionWrite(FILE *ofPtr, Data transfer) {
 
 void blackRecord(FILE *ofPTR, FILE *ofPTR_2, FILE *blackrecord, Data client_data, Data transfer) {
     while (fscanf(ofPTR, "%d%s%s%s%s%lf%lf%lf", &client_data.Number, client_data.Name, client_data.Surname,
-                  client_data.addres, client_data.TelNumber, &client_data.indebtedness, &client_data.credit_limit,
+                  client_data.address, client_data.TelNumber, &client_data.indebtedness, &client_data.credit_limit,
                   &client_data.cash_payments) != -1) {
         while (fscanf(ofPTR_2, "%d %lf", &transfer.Number, &transfer.cash_payments) != -1) {
             if ( client_data.Number == transfer.Number && transfer.cash_payments != 0 ) {
@@ -120,7 +118,7 @@ void blackRecord(FILE *ofPTR, FILE *ofPTR_2, FILE *blackrecord, Data client_data
             }
         }
         fprintf(blackrecord, "%-12d%-11s%-11s%-16s%20s%12.2f%12.2f%12.2f\n", client_data.Number, client_data.Name,
-                client_data.Surname, client_data.addres, client_data.TelNumber, client_data.indebtedness,
+                client_data.Surname, client_data.address, client_data.TelNumber, client_data.indebtedness,
                 client_data.credit_limit, client_data.cash_payments);
         rewind(ofPTR_2);
     }
