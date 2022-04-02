01e2f267826e44d8f1c4af35aa1645143bb2a080
Mikhail T <mihatjgl@mail.ru>
2022-04-02 18:23:28 +0300

    Made check for input

--- project/src/main.c
+++ project/src/main.c
@@ -6,8 +6,6 @@
 #include "transaction_write.h"
 #include "master_write.h"
 
-// TODO(me): проверка на null if (!c) {error}
-// TODO(me): проверка на считывание if (scanf() != *количество параметров*) {error}
 // TODO(me): написать тест
 
 enum buf_sizes {
@@ -15,12 +13,22 @@ enum buf_sizes {
     NAME = 20,
     SURNAME = 20,
     ADDRESS = 30,
-    TEL_NUMBER = 15
+    TEL_NUMBER = 15,
+
+    NUM_CLIENT_DATA_ARGS = 8,
+    NUM_TRANSFER_ARGS = 2,
+
+    ERR_WRONG_INPUT = -101,
+    ERR_WRONG_POINTER = -102
 };
 
-int input_client_data(data_t *Client) {
+int input_client_data(FILE *Ptr) {
+    if (!Ptr) {
+        return ERR_WRONG_POINTER;
+    }
+    data_t Client;
     char format_string[FORMAT_STRING_MAX_SIZE];
-    snprintf(format_string, FORMAT_STRING_MAX_SIZE, "%%i%%%ds%%%ds%%%ds%%%ds%%lf%%lf%%lf",
+    snprintf(format_string, FORMAT_STRING_MAX_SIZE, "%%i%%%ds%%%ds%%%ds%%%ds%%lf%%lf%%lf\n",
              NAME, SURNAME, ADDRESS, TEL_NUMBER);
     printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n",
            "1 Number account: ",
@@ -31,23 +39,42 @@ int input_client_data(data_t *Client) {
            "6 Client indebtedness: ",
            "7 Client credit limit: ",
            "8 Client cash payments: ");
-    int res = scanf(format_string, &Client->number, Client->name, Client->surname, Client->address,
-                    Client->tel_number, &Client->indebtedness, &Client->credit_limit, &Client->cash_payments);
-    return res;
+    int res = scanf(format_string, &Client.number, Client.name, Client.surname, Client.address,
+                    Client.tel_number, &Client.indebtedness, &Client.credit_limit, &Client.cash_payments);
+    while (res != -1) {
+        if (res != NUM_CLIENT_DATA_ARGS) {
+            return ERR_WRONG_INPUT;
+        }
+        master_write(Ptr, Client);
+        res = scanf(format_string, &Client.number, Client.name, Client.surname, Client.address,
+                    Client.tel_number, &Client.indebtedness, &Client.credit_limit, &Client.cash_payments);
+    }
+    return 0;
 }
 
-int input_transfer(data_t *transfer) {
+int input_transfer(FILE *Ptr) {
+    if (!Ptr) {
+        return ERR_WRONG_POINTER;
+    }
+    data_t Transfer;
     printf("%s\n%s\n",
            "1 Number account: ",
            "2 Client cash payments: ");
-    int res = scanf("%d %lf", &transfer->number, &transfer->cash_payments);
-    return res;
+    int res = scanf("%d %lf", &Transfer.number, &Transfer.cash_payments);
+    while (res != -1) {
+        if (res != NUM_TRANSFER_ARGS) {
+            return ERR_WRONG_INPUT;
+        }
+        transaction_write(Ptr, Transfer);
+        res = scanf("%d %lf", &Transfer.number, &Transfer.cash_payments);
+    }
+    return 0;
 }
 
 int main() {
     int choice = 0;
+    int res = 0;
     FILE *Ptr, *Ptr_2, *Blackrecord;
-    data_t client_data, transfer;
     printf("%s", "please enter action\n1 enter data client:\n2 enter data transaction:\n3 update base\n");
     while (scanf("%d", &choice) != -1) {
         switch (choice) {
@@ -56,10 +83,11 @@ int main() {
                 if ( Ptr == NULL ) {
                     puts("Have no access");
                 } else {
-                    while (input_client_data(&client_data) != -1) {
-                        master_write(Ptr, client_data);
-                    }
+                    res = input_client_data(Ptr);
                     fclose(Ptr);
+                    if (!res) {
+                        return res;
+                    }
                 }
                 break;
             case 2:
@@ -67,10 +95,11 @@ int main() {
                 if ( Ptr == NULL ) {
                     puts("Have no access");
                 } else {
-                    while (input_transfer(&transfer) != -1) {
-                        transaction_write(Ptr, transfer);
-                    }
+                    res = input_transfer(Ptr);
                     fclose(Ptr);
+                    if (!res) {
+                        return res;
+                    }
                 }
                 break;
             case 3:
@@ -93,5 +122,5 @@ int main() {
         }
         printf("%s", "please enter action\n1 enter data client:\n2 enter data transaction:\n3 update base\n");
     }
-    return 0;
+    return res;
 }
